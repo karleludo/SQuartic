8ab67265f95b0d839106ccbeecce137f5344878b
Karl Glenn Eludo <k_eludo@icloud.com>
2020-02-20 18:02:01 +0800

    admin-login page, added route, added post action

--- Project Main/index.js
+++ Project Main/index.js
@@ -1,15 +1,28 @@
-const express = require('express');
+const express = require('express'); //import express
+const bodyParser = require('body-parser'); //import bodyparser
+
 const app = express();
 
-
-app.listen(3000, (err)=> {
+app.use(bodyParser.urlencoded({  // bodyparser encode utf-8
+    extended: true
+}));
+//listen to port
+app.listen(3000, (err) => {
     if (err) console.log(err);
     else console.log('running successfully at port ' + 3000);
-    
 });
 
-
-app.get('/', (request,response) => {
+// get index 
+app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html');
-}) 
+});
 
+//get admin
+app.get('/admin', (request, response) => {
+    response.sendFile(__dirname + '/admin-login.html');
+});
+
+//post login credentials
+app.post('/login', (request, response) => {
+    console.log(request.body); //console test TODO: 
+});
