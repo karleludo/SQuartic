0de11777d9b64308449004fc470400dc8f2a0da3
Karl Glenn Eludo <k_eludo@icloud.com>
2020-07-19 03:41:03 +0800

    -added new styles. -fixed routing issues for css and scripts. -deleted old unused jpegs and svg's
    note: everything's working now

---
+++ Project Main/public/scripts/page1.js
@@ -0,0 +1,89 @@
+var submitted=false;
+
+//set current date mas mahaba jquery wiws di pa gumana
+document.querySelector("#date").valueAsDate = new Date();
+
+
+  
+
+var currentTab = 0; // Current tab is set to be the first tab (0)
+showTab(currentTab); // Display the current tab
+
+function showTab(n) {
+  // This function will display the specified tab of the form ...
+  var x = document.getElementsByClassName("tab");
+  x[n].style.display = "block";
+  // ... and fix the Previous/Next buttons:
+  if (n == 0) {
+    document.getElementById("prevBtn").style.display = "none";
+  } else {
+    document.getElementById("prevBtn").style.display = "inline";
+  }
+  if (n == (x.length - 1)) {
+    document.getElementById("nextBtn").innerHTML = "Finish Survey";
+    
+  } else {
+    document.getElementById("nextBtn").innerHTML = "Proceed to Survey";
+  }
+  // ... and run a function that displays the correct step indicator:
+  fixStepIndicator(n)
+}
+
+function nextPrev(n) {
+  // This function will figure out which tab to display
+  var x = document.getElementsByClassName("tab");
+  // Exit the function if any field in the current tab is invalid:
+  if (n == 1 && !validateForm()) return false;
+  // Hide the current tab:
+  x[currentTab].style.display = "none";
+  // Increase or decrease the current tab by 1:
+  currentTab = currentTab + n;
+  // if you have reached the end of the form... :
+  if (currentTab >= x.length) {
+    //...the form gets submitted:
+    showAlert();
+    document.getElementById("regForm").submit();
+    window.location ="page1.html"
+    return false;
+  }
+  // Otherwise, display the correct tab:
+  showTab(currentTab);
+}
+
+function validateForm() {
+  // This function deals with validation of the form fields
+  var x, y, i, valid = true;
+  x = document.getElementsByClassName("tab");
+  y = x[currentTab].querySelectorAll("input:not([type='tel']");
+  // y = x[currentTab].document.querySelectorAll("input:not([type='tel'],input[type='checkbox'],input[type='radio']");
+  // FIXME dpat macheck rin yung input type radio ano ba laman ng x at y
+  // A loop that checks every input field in the current tab:
+  for (i = 0; i < y.length; i++) {
+    // If a field is empty...
+    if (y[i].value == "") {
+      // add an "invalid" class to the field:
+      y[i].className += " invalid";
+      // and set the current valid status to false:
+      valid = false;
+    }
+  }
+  // If the valid status is true, mark the step as finished and valid:
+  if (valid) {
+    document.getElementsByClassName("step")[currentTab].className += " finish";
+  }
+  return valid; // return the valid status
+}
+
+function fixStepIndicator(n) {
+  // This function removes the "active" class of all steps...
+  var i, x = document.getElementsByClassName("step");
+  for (i = 0; i < x.length; i++) {
+    x[i].className = x[i].className.replace(" active", "");
+  }
+  //... and adds the "active" class to the current step:
+  x[n].className += " active";
+}
+
+function showAlert(){
+    alert("Thank you for answering the survey!");
+}
