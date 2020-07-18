9b7f96d5acc33a53c5abd02386ee4c4bf4f7e381
Karl Glenn Eludo <k_eludo@icloud.com>
2020-07-19 02:54:25 +0800

    deleted unused routes

--- Project Main/routes/index.js
+++ Project Main/routes/index.js
@@ -2,40 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // response.render('index');  changed to landing.ejs from matt
   res.render('landing');
 });
-
 router.get('/page1', (req, res) => {
   res.render('page1');
 });
-
-router.get('/page2', (req, res) => {
-  res.render('page2');
-});
-
-router.get('/page3', (req, res) => {
-  res.render('page3');
-});
-
-router.get('/page4', (req, res) => {
-  res.render('page4');
-});
-
-router.get('/finish', (req, res) => {
-  res.render('finish');
-});
-
-
-router.post('/page2', (req,res) => {
-  res.render('page2');
-});
-
 router.get('/test', (req, res) => {
   res.render('test');
 });
 
-
+//unfinished code (formgenerator)
 const createStudent = require(process.cwd() + '/modules/user-model/Student.js').createStudent;
 const createForm = require(process.cwd() + '/modules/user-model/Form.js').createForm;
 
