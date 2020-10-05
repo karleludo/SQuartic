aea3a9166df17115043ba85788a02c2951692f6f
Karl Eludo <k_eludo@icloud.com>
2020-10-05 17:55:09 +0800

    simple comment instructions for creating Admin credentials, uncommented code TODO: This should be modularized

--- index.js
+++ index.js
@@ -1,36 +1,40 @@
-const express = require('express'); // import express
-const bodyParser = require('body-parser'); // import bodyparser
-const ejs = require('ejs');
-const cookieSession = require('cookie-session');
+const express = require("express"); // import express
+const bodyParser = require("body-parser"); // import bodyparser
+const ejs = require("ejs");
+const cookieSession = require("cookie-session");
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-dbInit = require(__dirname + '/modules/db-connection/MongoDB.js').dbInit;
+dbInit = require(__dirname + "/modules/db-connection/MongoDB.js").dbInit;
 
 // initialise db connection
 dbInit();
 
 //cookieSession
-app.use(cookieSession({
-  name: 'session',
-  secret: 'secret'
-}));
+app.use(
+  cookieSession({
+    name: "session",
+    secret: "secret",
+  })
+);
 
 // bodyparser encode utf-8
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 // images, css, and client side javascript files are located here
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-const port = (process.env.PORT) ? process.env.PORT : 3000;
+const port = process.env.PORT ? process.env.PORT : 3000;
 
 // listen to port
 app.listen(port, (err) => {
   if (err) console.log(err);
-  else console.log('running successfully at port ' + port);
+  else console.log("running successfully at port " + port);
 });
 
 // rerouted routes to different files for easier coding
@@ -38,24 +42,26 @@ app.listen(port, (err) => {
 // eg. admin.js would have admin specific routes such as /admin and /admin/logout
 
 //  new index route
-app.use('/', require(process.cwd() + '/routes/index.js'));
+app.use("/", require(process.cwd() + "/routes/index.js"));
 
 // new admin route
-app.use('/admin', require(process.cwd() + '/routes/admin.js'));
+app.use("/admin", require(process.cwd() + "/routes/admin.js"));
 
+// create admin account PROTOTYPE
+// TODO: modularize
+// description: The following block is used to create test account credentials for Admin login "/admin" page
 
-// create admin account test
-// const createUser = require(__dirname + '/modules/user-model/Admin.js').createUser;
-//
-// let admin = createUser('testAdminName', 'testPassword'); // test account
-
+const createUser = require(__dirname + "/modules/user-model/Admin.js")
+  .createUser;
+let admin = createUser("testAdminName", "testPassword"); // test account credentials
 // upload admin credentials to db
-// TODO: bcrypt password
 
 // run 'mongod' before running 'node index.js'
 // uncomment and run once to save admin credentials to db
 
-// admin.save((err) => {
-//   if(err) console.log(err);
-//   else console.log(admin.username + ' saved successfully');
-// }); // run this once to save admin to db then delete this code
+// save credentials to DB
+admin.save((err) => {
+  if (err) console.log(err);
+  else console.log(admin.username + " saved successfully");
+});
+// run this once to save admin to db then delete this code
